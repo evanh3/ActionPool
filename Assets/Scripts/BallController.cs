691ee442d21d47520720a8871a53f0616ec3943d
Evan Hembacher <evanh3@gmail.com>
2016-09-14 00:48:55 -0700

    -Finished mouse input that causes ball movement -Started ball spin feature

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,56 +4,82 @@ using System.Collections;
 public class BallController : MonoBehaviour {
 
 	public int speed;
-	public Vector3 spin;
-	private bool mouseHeldDown = false;
-	private bool touchingBallCondition = true;
+	public int spinSpeed;
+	private bool mouseHeldDown;
 	private Vector3 initialMouseLocation;
 	private Vector3 finalMouseLocation;
 	private Vector3 mouseDistTraveled;
+	private Vector3 ballMoveVector;
+
+	private bool moveBall;
 
 	private float throwSpeed;
 	private float throwSpin;
 
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		mouseHeldDown = false;
+		rb = GetComponent<Rigidbody> ();
+		moveBall = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// getMouseMovement ();
+		// print((Input.GetAxis ("Mouse X")).ToString());
 
 		//detects whether the mouse is clicked
 		if (Input.GetMouseButtonDown (0)) {
 			print ("clicked this frame");
 			mouseHeldDown = true;
 			initialMouseLocation = Input.mousePosition;
-
-			//detects whether the mouse is over the ball
-			if (touchingBallCondition) {
-				print ("clicked on ball");
-			}
 		} 
 
 		//detects whether mouse is unclicked
 		else if (Input.GetMouseButtonUp (0)) {
 			print ("unclicked this frame");
+			print ("Final mouse location: " + (mouseDistTraveled.ToString()));
 			mouseHeldDown = false;
 			finalMouseLocation = Input.mousePosition;
-			rollBall ();
+			moveBall = true;
 		}
 
 		//do this if the mouse is still held down
 		if (mouseHeldDown) {
-			print ("******still held down this frame******");
+			finalMouseLocation = Input.mousePosition;
+			mouseDistTraveled = initialMouseLocation - finalMouseLocation;
+			//print ("******still held down this frame******");
+			print (mouseDistTraveled.ToString());
 		}
 	}
 
 	// Fixed Update is called once per physics calculation
 	void FixedUpdate() {
+		if (moveBall) {
+			rollBall ();
+		}
+		moveBall = false;
+
 
 	}
 
+	void rollBall(){
+		mouseDistTraveled = initialMouseLocation - finalMouseLocation;
+		ballMoveVector.x = mouseDistTraveled.x;
+		ballMoveVector.z = mouseDistTraveled.y;
+		ballMoveVector.y = 0;
+
+		//rb.AddForce (ballMoveVector);
+		rb.AddRelativeTorque (0, 0, ballMoveVector.x * spinSpeed * (-1));
+
+		ballMoveVector = Vector3.zero;
+
+		throwSpeed = mouseDistTraveled.x;
+		throwSpin = mouseDistTraveled.y;
+	}
+
+	/*
 	void getMouseMovement(){
 		if (Input.GetAxis ("Mouse X") < 0) {
 			print ("left");
@@ -68,10 +94,6 @@ public class BallController : MonoBehaviour {
 			print ("up");
 		}
 	}
+	*/
 
-	void rollBall(){
-		mouseDistTraveled = initialMouseLocation - finalMouseLocation;
-		throwSpeed = mouseDistTraveled.x;
-		throwSpin = mouseDistTraveled.y;
-	}
 }
